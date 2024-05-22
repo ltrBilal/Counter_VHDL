798d8e815d0308d69f882a46c8d9bcdde33d4437
ltrBilal <156922513+ltrBilal@users.noreply.github.com>
2024-05-22 14:20:25 +0200

    add comments

--- generate_VHDL.py
+++ generate_VHDL.py
@@ -2,8 +2,8 @@ import subprocess
 from functions import *
 
 # scripts
-script_path = "./counter.py"
-expected_results = "./expected_results.py"
+script_path = "./counter.py" # script to generate VHDL file
+expected_results = "./expected_results.py" # script to generate expected results
 
 # files
 results_file = "./output_file.txt"
@@ -12,7 +12,7 @@ expected_file = "./expected_file.txt"
 
 i = 0
 while i < 200:
-    args = [f"{i + 35}"]
+    args = [f"{i + 35}"] # parametre
     i = i + 25
     # generate VHDL code
     result = subprocess.run(["python3", script_path] + args, capture_output=True, text=True)
@@ -23,7 +23,7 @@ while i < 200:
     print(expected.stdout)
 
     # execute VHDL code
-    subprocess.run(["make"], check=True)
+    subprocess.run(["make"], check=True) # GHDL Makefile
     
     # compare results
     difference_position = find_first_difference(results_file, expected_file)
